528f71521ccb3f2288acfe4d24e237b640f2f616
Rahulgit3D2Y <rc2504000@gmail.com>
2023-09-18 17:42:36 +0530

    1

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,29 @@
+import { Search } from '@mui/icons-material'
+import { IconButton, Paper } from '@mui/material'
+import React from 'react'
+
+const Searchbar = () => {
+  return (
+    <Paper
+    component="form"
+    onSubmit={()=>{}}
+    sx={{
+      borderRadius:20,
+      border:'1px solid #e3e3e3',
+      pl:2,boxShadow:'none',
+      mr:{sm:5}
+    }}><input 
+    className='Search-bar'
+    placeholder='Search...'
+    value=''
+     
+    onChange={()=>{}} style={{border:'none'}}/>
+    <IconButton type='submit' sx={{p:'10px',color:'red'}}>
+      <Search  />
+    </IconButton>
+    </Paper>
+
+  )
+}
+
+export default Searchbar
